21bd081c27fd5990e52ba60dadb92205016635c0
jimmyroot <jimmybnewman@gmail.com>
2023-11-28 16:34:59 +0000

    Mostly complete, need to add filled heart

---
+++ index.js
@@ -0,0 +1,111 @@
+const posts = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+        liked: false
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed tbh",
+        likes: 4,
+        liked: false
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        liked: false
+    }
+]
+
+// Init
+const mainEl = document.getElementById('container-all-posts')
+
+document.addEventListener('click', (event) => {
+    // console.log(event.target)
+    const operation = event.target.dataset.operation
+    if (operation === 'like') {
+        // Set up variables
+        const likedPostID = event.target.dataset.postid
+        const likedPostLikesEl = document.getElementById(`likes-for-post${likedPostID}`)
+        let likes = posts[likedPostID].likes
+        let liked = posts[likedPostID].liked
+        
+        // Toggle liked
+        liked = !liked
+        
+        // Increment or decrement depending on if we liked or unliked
+        liked ? likes++ : likes--
+
+        // Update the DOM
+        likedPostLikesEl.textContent = `${likes} likes` 
+        posts[likedPostID].likes = likes
+        posts[likedPostID].liked = liked
+        console.log(posts[likedPostID].likes, posts[likedPostID].liked)
+    }
+})
+
+function renderPosts() {
+    for (const [index, post] of posts.entries()) {
+        const html = `
+            <div class="container-single-post">
+                            <div class="header-post">
+                                <img class="avatar-user" src="${post.avatar}">
+                                <div>
+                                    <p class="p-user-data">
+                                        <span class="bold">
+                                            ${post.name}
+                                        </span>
+                                        <span class="location">
+                                            ${post.location}
+                                        </span>
+                                    </p>
+                                </div>
+                            </div>
+                            
+                            <img class="img-post" src="${post.post}">
+                            
+                            <div class="footer-post">
+                                <div class="container-buttons">
+                                    <img 
+                                        src="images/icon-heart.png" 
+                                        class="btn btn-like" 
+                                        data-operation=like
+                                        data-postid=${index}
+                                    >
+                                        <img src="images/icon-comment.png" class="btn">
+                                    <img src="images/icon-dm.png" class="btn">
+                                </div>
+                                <div>
+                                    <p 
+                                        class="p-footer bold" 
+                                        id="likes-for-post${index}"
+                                    > 
+                                        ${post.likes} likes
+                                    </p>
+                                    <p class="p-footer">
+                                        <span class="bold">
+                                            ${post.username}&nbsp;
+                                        </span>
+                                        ${post.comment}
+                                    </p>
+                                </div>
+                            </div>
+                    </div>
+        `
+        mainEl.innerHTML += html
+    }
+}
